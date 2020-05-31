4ec4a7e7783e5f3b9135e7b24ccb4b011af2f234
Edward Knowles <ed.knowles@stunjelly.com>
2020-05-31 17:10:20 +0100

    update to ts

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>300.team</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1>
+        Welcome to <a href="https://300.team">300.team!</a>
+      </h1>
+    </main>
+  </div>
+)
+
+export default Home;
