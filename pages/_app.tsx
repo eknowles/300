9fa42b5a6898981035eac5836109f62f51b297d5
Edward Knowles <ed.knowles@stunjelly.com>
2020-06-02 18:05:10 +0100

    add google site verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <meta name="google-site-verification" content="jJEM2HJi1AU5A39kYqPtHVPzsyeRIJUhYNGFNFk1jeQ" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
