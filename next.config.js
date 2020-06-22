57d83eacdc58ad08ab24f8b5ffaa132a57d615bd
Edward Knowles <ed.knowles@stunjelly.com>
2020-06-22 08:59:15 +0100

    fix: env vars for next

--- next.config.js
+++ next.config.js
@@ -35,6 +35,7 @@ module.exports = (phase) => {
       IS_DEV: isDev,
       IS_PROD: isProd,
       IS_STAGING: isStaging,
+      FAUNADB_SECRET: process.env.FAUNADB_SECRET,
       DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
       STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
       DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
